68bd17d5a4f27bd99dcc61d694d2d04f3a7e3ae9
Hephzy <hzdelight01@gmail.com>
2025-06-30 20:39:14 +0100

    Deleted package-lock.json

--- backend/src/routes/expenses.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { createExpense, getAllExpenses } from '../handlers/expense';
-
-const route = Router();
-
-route.post('/create', createExpense);
-route.get('/', getAllExpenses);
-
-export default route;
--- backend/src/utils/appResponse.ts
+++
@@ -1,9 +0,0 @@
-import { Response } from 'express';
-
-export const appResponse = (res: Response, statusCode: number, message: string, data?: any | string | null) => {
-  return res.status(statusCode).json({
-    status: 'success',
-    message: message ?? 'Operation completed successfully',
-    data: data ?? null
-  });
-}
